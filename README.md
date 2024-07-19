83b2e7de79721c61e00c15b761003ecd3d16c251
Eugene Zhikharev <janiqgames@gmail.com>
2024-07-19 12:12:24 +0700

    configurating projects

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/subthored/php-project-9/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/subthored/php-project-9/actions)
+
+### Link to Render.Com:
+[Redner.app](https://php-project-9-q5n0.onrender.com)
--- public/index.php
+++ public/index.php
@@ -3,14 +3,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
-use Slim\Middleware\MethodOverrideMiddleware;
 use DI\Container;
-
-$app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
 
 session_start();
 
-$app->get('/', function ($request, $response){
-    return $response->write("<h1>Hello</h1>");
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    $params = [];
+    return $this->get('renderer')->render($response, 'index.html', $params);
 });
