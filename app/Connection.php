be3773f8f3b62f73aae067a7adf5ec90486a5201
Eugene Zhikharev <janiqgames@gmail.com>
2024-07-30 13:06:37 +0700

    adding connection to database

--- app/Connection.php
+++ app/Connection.php
@@ -8,7 +8,19 @@ class Connection
 
     public function connect()
     {
-        $params = parse_ini_file('database.ini');
+        if (getenv('DATABASE_URL')) {
+            $databaseUrl = parse_url(getenv('DATABASE_URL'));
+        }
+
+        if (isset($databaseUrl['host'])) {
+            $params['host'] = $databaseUrl['host'];
+            $params['port'] = $databaseUrl['port'] ?? null;
+            $params['database'] = $databaseUrl['path'] ?? null;
+            $params['user'] = $databaseUrl['user'] ?? null;
+            $params['password'] = $databaseUrl['pass'] ?? null;
+        } else {
+            $params = parse_ini_file('database.ini');
+        }
         if ($params === false) {
             throw new \Exception("Error reading database configuration file");
         }
