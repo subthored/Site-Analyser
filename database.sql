a7fcde06a6171c9f2dd99a31569a8b6b80ac51e9
Eugene Zhikharev <janiqgames@gmail.com>
2024-07-30 14:49:53 +0700

    new files for database

---
+++ database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS urls
+(
+    id         bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name       varchar (255) UNIQUE NOT NULL,
+    created_at timestamp
+    );
+
+CREATE TABLE IF NOT EXISTS url_checks
+(
+    id          bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id      bigint NOT NULL,
+    status_code int,
+    h1          varchar (255),
+    title       varchar (255),
+    description text,
+    created_at  timestamp
+    );
